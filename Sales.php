7f29bf005189dd3b1395d1d5c9d5faf415c693ae
chuman <chumanashrestha@gmail.com>
2021-04-02 01:01:49 +0545

    sales.php is divided

--- Sales.php
+++ Sales.php
@@ -1,56 +1,6 @@
 <?php
 include("ses_check.php");
 include("dbconnection.php");
-if (isset($_POST['btn_save'])) {
-    date_default_timezone_set('Asia/Kathmandu');
-    extract($_POST);
-    //echo '<script type="text/javascript">alert("' .  $customer_id ." ".$customer_name. '"); </script>';
-    $build_date=date('Y-m-d h:i:sa');
-    $sql_insert ="INSERT INTO `sales`(`date`, `f_price`, `remark`) VALUES ('$build_date','$subtotal','$remark')";//final sales report
-    $res_insert = $conn->query($sql_insert);
-    $last_sales_id =  mysqli_insert_id($conn);//increment from 1 by last data of column with AUTO INCREMENT
-    $sales_id = $last_sales_id;
-
-/*****************************************************************************************/
-    $service = count($_POST['select_services']);
-    echo '<script type="text/javascript">alert("' ."no of selection = $service".'");</script>';
-    for ($i=0;$i<$service;$i++) {
-        $sql_service = "INSERT into prod_sales(`sales_id`,`prod_id`, `qty`, `t_price`)values('$sales_id','$select_services[$i]','$quantity[$i]','$total[$i]')";
-        //echo '<script type="text/javascript">alert("' .$select_services[$i].'");</script>';
-        $conn->query($sql_service);
-    }
-/************************************************************************************************/
-    if ($customer_name!=="") {//new member
-      $sql_ctmr="INSERT INTO `customer` (`cname`,`reg_date`,`t_buy`) VALUES ('$customer_name','$build_date',$subtotal)";
-      $result_ctmr=$conn->query($sql_ctmr);
-      $sql_main="SELECT * from `customer` where `cname`='$customer_name' AND  `reg_date`='$build_date'";
-      $result_main=$conn->query($sql_main);
-      //echo '<script type="text/javascript">alert("' ."$build_date".'");</script>';
-      if ($result_main->num_rows > 0) {
-        while($row = $result_main->fetch_assoc()) {
-          echo '<script type="text/javascript">alert("'.$row['csid'].'");</script>';
-          $customer_id=$row['csid'];
-        }
-      }
-    }
-    else {//old member
-      $sql_main="SELECT * from `customer` where `csid`=$customer_id";
-      $result_main=$conn->query($sql_main);
-      if ($result_main->num_rows > 0) {
-        while($row = $result_main->fetch_assoc()) {
-          $subtotal=$row['t_buy']+$subtotal;
-          //echo '<script type="text/javascript">alert("'.$row['cname'] ." ".$subtotal.'");</script>';
-          $sql_ctmr="UPDATE `customer` SET `t_buy`=$subtotal WHERE `csid` = $customer_id ";
-          $result_ctmr=$conn->query($sql_ctmr);
-        }
-      }
-    }
-/********************************************************************************************************/
-  $sql_ctmr_sales="INSERT INTO `ctmr_sales`(`cid`, `sales_id`) VALUES ($customer_id,$sales_id)";
-  $conn->query($sql_ctmr_sales);
-/*************************************************************************************************************/
-
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -93,13 +43,13 @@ if (isset($_POST['btn_save'])) {
     </ul>
   </div>
   <div class="body">
-    <form class="form-valide" method="POST" action="" name="userform" enctype="multipart/form-data">
+    <form class="form-valide" method="POST" action="willbill.php" name="userform" enctype="multipart/form-data">
       <div class="row">
         <div class="form-group row col-md-6">
-          <label class="col-sm-3 control-label">Billed Date:</label>
+          <label class=" control-label">Billed Date:</label>
           <input type="date" name="build_date" class="form-control datepicker" value="<?=date('Y-m-d')?>" data-provide="datepicker">
         </div>
-        <!--*******************************************************************-->
+        <!--------------------------------------------------------------------------------->
         <div id="prag2">
           <input type="radio" name="alt_btn" value="" onclick="changeme('2');" required>New Member
           <input type="radio" name="alt_btn" value="" onclick="changeme('1');">Old Member
@@ -108,7 +58,7 @@ if (isset($_POST['btn_save'])) {
         <!------------------------------------------------------------------------->
       </div>
       <div class="form-group row">
-        <div class="col-sm-3">
+        <div class="">
             Product
         </div>
         <div class="col-sm-2">
@@ -123,7 +73,7 @@ if (isset($_POST['btn_save'])) {
       </div>
       <div class="mydiv">
         <div class="form-group row control-group after-add-more subdiv">
-          <div class="col-sm-3">
+          <div class="">
             <select name="select_services[]" class="form-control select_services" required>
               <option value="" selected="true" disabled="disabled">--SelectProduct--</option>
               <?php
@@ -146,7 +96,7 @@ if (isset($_POST['btn_save'])) {
           </div>
 
           <div class="col-sm-2">
-            <button class="btn btn-success add-more" type="button"></i> Add</button>
+            <button class="btn  add-more" type="button"></i> Add</button>
           </div>
         </div>
 
@@ -154,14 +104,14 @@ if (isset($_POST['btn_save'])) {
       <!-- mydiv -->
 
       <div class="form-group row">
-        <label class="col-sm-6 control-label">Total</label>
-        <div class="col-sm-3">
+        <label class=" control-label">Total</label>
+        <div class="">
           <input type="text" name="subtotal" id="subtotal" class="form-control" placeholder="Subtotal" readonly="">
         </div>
       </div>
       <div class="form-group row">
-        <label class="col-sm-6 control-label">Remark</label>
-        <div class="col-sm-3">
+        <label class=" control-label">Remark</label>
+        <div class="">
           <input type="text" name="remark" id="remark" class="form-control" placeholder="Remark">
         </div>
       </div>
@@ -170,7 +120,7 @@ if (isset($_POST['btn_save'])) {
     <div class="copy hide">
       <div class="form-group control-group row subdiv">
 
-        <div class="col-sm-3">
+        <div class="">
             <select name="select_services[]" class="form-control select_services">
               <option value="">--SelectProduct--</option>
               <?php
---
+++ willbill.php
@@ -0,0 +1,60 @@
+<?php
+include("ses_check.php");
+include("dbconnection.php");
+
+
+if (isset($_POST['btn_save'])) {
+    date_default_timezone_set('Asia/Kathmandu');
+    extract($_POST);
+    //echo '<script type="text/javascript">alert("' .  $customer_id ." ".$customer_name. '"); </script>';
+    $build_date=date('Y-m-d h:i:sa');
+    $sql_insert ="INSERT INTO `sales`(`date`, `f_price`, `remark`) VALUES ('$build_date','$subtotal','$remark')";//final sales report
+    $res_insert = $conn->query($sql_insert);
+    $last_sales_id =  mysqli_insert_id($conn);//increment from 1 by last data of column with AUTO INCREMENT
+    $sales_id = $last_sales_id;
+
+/*****************************************************************************************/
+    $service = count($_POST['select_services']);
+    echo '<script type="text/javascript">alert("' ."no of selection = $service".'");</script>';
+    for ($i=0;$i<$service;$i++) {
+        $sql_service = "INSERT into prod_sales(`sales_id`,`prod_id`, `qty`, `t_price`)values('$sales_id','$select_services[$i]','$quantity[$i]','$total[$i]')";
+        //echo '<script type="text/javascript">alert("' .$select_services[$i].'");</script>';
+        $conn->query($sql_service);
+    }
+/************************************************************************************************/
+    if ($customer_name!=="") {//new member
+      $sql_ctmr="INSERT INTO `customer` (`cname`,`reg_date`,`t_buy`) VALUES ('$customer_name','$build_date',$subtotal)";
+      $result_ctmr=$conn->query($sql_ctmr);
+      $sql_main="SELECT * from `customer` where `cname`='$customer_name' AND  `reg_date`='$build_date'";
+      $result_main=$conn->query($sql_main);
+      //echo '<script type="text/javascript">alert("' ."$build_date".'");</script>';
+      if ($result_main->num_rows > 0) {
+        while($row = $result_main->fetch_assoc()) {
+          echo '<script type="text/javascript">alert("'.$row['csid'].'");</script>';
+          $customer_id=$row['csid'];
+        }
+      }
+    }
+    else {//old member
+      $sql_main="SELECT * from `customer` where `csid`=$customer_id";
+      $result_main=$conn->query($sql_main);
+      if ($result_main->num_rows > 0) {
+        while($row = $result_main->fetch_assoc()) {
+          $subtotal=$row['t_buy']+$subtotal;
+          //echo '<script type="text/javascript">alert("'.$row['cname'] ." ".$subtotal.'");</script>';
+          $sql_ctmr="UPDATE `customer` SET `t_buy`=$subtotal WHERE `csid` = $customer_id ";
+          $result_ctmr=$conn->query($sql_ctmr);
+        }
+      }
+    }
+/********************************************************************************************************/
+  $sql_ctmr_sales="INSERT INTO `ctmr_sales`(`cid`, `sales_id`) VALUES ($customer_id,$sales_id)";
+  $conn->query($sql_ctmr_sales);
+/*************************************************************************************************************/
+  header('location:bill.php');
+}else {
+  header('location:sales.php');
+}
+
+
+?>
