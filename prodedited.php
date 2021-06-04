76ab406297bf44a2de541a8e25656eb06952409f
chuman <chumanashrestha@gmail.com>
2021-06-04 06:10:34 +0545

    return path set

--- prodedited.php
+++ prodedited.php
@@ -3,5 +3,5 @@ include("dbconnection.php");
 
 $sql="UPDATE products set name='".$_POST['name']."', cat_id='".$_POST['cat_id']."', quantity='".$_POST['quantity']."', buy_price='".$_POST['buy_price']."', sale_price='".$_POST['sale_price']."' where id='".$_GET['id']."'";
 $result=$conn->query($sql);
-header('location:user.php');
+header('location:product.php');
 ?>
