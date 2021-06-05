07b3be829e7408dd9b7110e331704ecc1b03833e
chuman <chumanashrestha@gmail.com>
2021-06-05 15:18:54 +0545

    method changes

--- catedited.php
+++ catedited.php
@@ -2,7 +2,7 @@
 include("ses_check.php");
 include("dbconnection.php");
 
-$sql="UPDATE categories  set name='".$_POST['name']."' where id = '".$_POST['id']."'";
+$sql="UPDATE categories  set name='".$_POST['name']."' where id = '".$_GET['id']."'";
 $result=$conn->query($sql);
 
 header('location:categories.php');
