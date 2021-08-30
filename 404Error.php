2d3df553e1677ace53d91ca8592fdf95e7fbc279
chuman <chumanashrestha@gmail.com>
2021-08-30 19:56:09 +0545

    404 page new design

--- 404Error.php
+++ 404Error.php
@@ -10,6 +10,34 @@
   <title>Document</title>
 </head>
 <body>
-  <h1>404 ok</h1>
+  <h1>Page Not Found</h1>
+  <h1>404 Error</h1>
+  <h2 style="color:red">Something Went Wrong</h2>
+  <h4><a href="index.php">← Return To Dashboard</h4>
 </body>
+<style>
+	body{
+		background-color:lightgreen;
+		background: linear-gradient(to left, black, white, black);
+		padding-top:20vh;
+	}
+	h1, h2, h3, h4{
+		text-align: center;
+	}
+	a:link {
+	  text-decoration: none;
+	  
+	}
+	a:visited {
+	  text-decoration: none;
+	}
+
+	a:hover {
+	  text-decoration: none;
+	}
+
+	a:active {
+	  text-decoration: underline;
+	}
+</style>
 </html>
