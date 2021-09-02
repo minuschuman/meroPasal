1d21ee4e69f342dabc1513f8a3c0a0d8a4a8f0e1
chuman <chumanashrestha@gmail.com>
2021-09-02 22:19:26 +0545

    well-formated

--- delprod.php
+++ delprod.php
@@ -4,13 +4,11 @@ include("dbconnection.php");
 
 $sql="UPDATE products  set delete_status='1' where id='".$_GET['id']."'";
 echo 'Deleted successfully.';
-if($conn->query($sql))
-{
-  ?>
+if ($conn->query($sql)) {
+    ?>
   	<script>alert ("Record Deleted");</script>
   <?php
   header('location:product.php');
-}else
-{
-	echo "Not Deleted";
+} else {
+    echo "Not Deleted";
 }
